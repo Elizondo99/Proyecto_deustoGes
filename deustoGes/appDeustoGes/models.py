97feb3a7da67cf8c4b7dbe09a44530fb3e839f03
Jon Rodriguez <jon.r.echaniz@opendeusto.es>
2024-04-19 00:36:19 +0200

    añadir vista indexar y mostrar empleados, pendiente de verificacion. intento de añadir informacion a la base de datos. crear html empleados_detail y empleados_index, no rellenar por dentro

--- deustoGes/appDeustoGes/models.py
+++ deustoGes/appDeustoGes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Cliente(models.Model):
@@ -11,22 +12,31 @@ class Cliente(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        verbose_name = "cliente"
+        verbose_name_plural = "clientes"
+
 
 class Empleado(models.Model):
-
     dni = models.CharField(max_length=9)
     nombre = models.CharField(max_length=15)
-    apellidos = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
     email = models.EmailField()
     telefono = models.IntegerField()
     responsable = models.BooleanField(default=False)
 
-
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-class Proyecto(models.Model):
+    def __str__(self):
+        return self.nombre + " " + self.apellido
 
+    class Meta:
+        verbose_name = "empleado"
+        verbose_name_plural = "empleados"
+
+
+class Proyecto(models.Model):
     nombre = models.CharField(max_length=15)
     descripcion = models.TextField()
     fecha_inicio = models.DateField()
@@ -34,22 +44,29 @@ class Proyecto(models.Model):
     presupuesto = models.IntegerField()
     tareas = models.TextField(blank=True)
 
-    miembros = models.ManyToManyField(Empleado, blank=True)
-    responsable  = models.ForeignKey(Empleado, on_delete=models.CASCADE)
-    cliente =models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    # miembros = models.ManyToManyField(Empleado, blank=True)
+    responsable = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.nombre + " (Responsable: " + self.responsable + ")"
+
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+
 
 class Tarea(models.Model):
-
     nombre = models.CharField(max_length=15)
     descripcion = models.TextField()
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField()
     prioridad = models.CharField(max_length=10, choices=(('alta', 'Alta'), ('media', 'Media'), ('baja', 'Baja')))
-    estado = models.CharField(max_length=20, choices=(('abierta', 'Abierta'), ('asignada', 'Asignada'), ('en_proceso', 'En proceso'), ('finalizada', 'Finalizada')))
+    estado = models.CharField(max_length=20, choices=(
+        ('abierta', 'Abierta'), ('asignada', 'Asignada'), ('en_proceso', 'En proceso'), ('finalizada', 'Finalizada')))
     notas_adicionales = models.TextField()
 
     proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
@@ -57,3 +74,10 @@ class Tarea(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre + " (prioridad: " + str(self.prioridad) + ")"
+
+    class Meta:
+        verbose_name = 'tarea'
+        verbose_name_plural = 'tareas'
