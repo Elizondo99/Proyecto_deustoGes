c64e8b69c801b00d112c9aa56d789052a33a8ca0
Elizondo99 <josuelizondo@opendeusto.es>
2024-04-23 18:45:03 +0200

    datos en BBDD y prueba superuser fallida

--- deustoGes/appDeustoGes/admin.py
+++ deustoGes/appDeustoGes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from appDeustoGes.models import Cliente, Proyecto, Empleado, Tarea
+
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Proyecto)
+admin.site.register(Empleado)
+admin.site.register(Tarea)
