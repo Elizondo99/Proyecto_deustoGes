1ac8f6b776723f1049fb4d5ef6b58d1a47f68009
Jon Rodriguez <jon.r.echaniz@opendeusto.es>
2024-05-26 18:26:34 +0200

    cambio de relacion entre proyecto y tarea

--- deustoGes/appDeustoGes/models.py
+++ deustoGes/appDeustoGes/models.py
@@ -99,30 +99,6 @@ class Empleado(models.Model):
 """
 
 
-# Modelo de la entidad Tarea.
-class Tarea(models.Model):
-    nombre = models.CharField(max_length=15)
-    descripcion = models.TextField()
-    fecha_inicio = models.DateField()
-    fecha_fin = models.DateField()
-    prioridad = models.CharField(max_length=10, choices=(('alta', 'Alta'), ('media', 'Media'), ('baja', 'Baja')))
-    estado = models.CharField(max_length=20, choices=(
-        ('abierta', 'Abierta'), ('asignada', 'Asignada'), ('en_proceso', 'En proceso'), ('finalizada', 'Finalizada')))
-    notas_adicionales = models.TextField()
-
-    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
-
-    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
-
-    def __str__(self):
-        return str(self.nombre) + " (prioridad: " + str(self.prioridad) + ")"
-
-    class Meta:
-        verbose_name = 'tarea'
-        verbose_name_plural = 'tareas'
-
-
 # Modelo de la entidad Proyecto.
 class Proyecto(models.Model):
     nombre = models.CharField(max_length=15)
@@ -130,7 +106,6 @@ class Proyecto(models.Model):
     fecha_inicio = models.DateField(default=date(2020, 1, 1))
     fecha_fin = models.DateField(default=date(2020, 1, 1))
     presupuesto = models.IntegerField()
-    tareas = models.ManyToManyField(Tarea, blank=False)
 
     # miembros = models.ManyToManyField(Empleado, blank=True)
     responsable = models.ForeignKey(Empleado, on_delete=models.CASCADE)
@@ -145,6 +120,31 @@ class Proyecto(models.Model):
     class Meta:
         verbose_name = "proyecto"
         verbose_name_plural = "proyectos"
+
+
+# Modelo de la entidad Tarea.
+class Tarea(models.Model):
+    nombre = models.CharField(max_length=15)
+    descripcion = models.TextField()
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    prioridad = models.CharField(max_length=10, choices=(('alta', 'Alta'), ('media', 'Media'), ('baja', 'Baja')))
+    estado = models.CharField(max_length=20, choices=(
+        ('abierta', 'Abierta'), ('asignada', 'Asignada'), ('en_proceso', 'En proceso'), ('finalizada', 'Finalizada')))
+    notas_adicionales = models.TextField()
+
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
+
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.nombre) + " (prioridad: " + str(self.prioridad) + ")"
+
+    class Meta:
+        verbose_name = 'tarea'
+        verbose_name_plural = 'tareas'
 
 
 # Modelo de la entidad Solicitud.
